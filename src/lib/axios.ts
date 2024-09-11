dc77a72405345ea762de382c6bc3f99531366301
Naiara Martins <naiaraxf@gmail.com>
2024-09-11 11:09:15 -0300

    Update environment baseURL in lib/axios

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -3,5 +3,5 @@ import axios from "axios";
 console.log('API URL:', process.env.REACT_APP_API_URL);
 
 export const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL,
+  baseURL: process.env.REACT_APP_API_URL || "https://naiara-jobber-backend-477bded58652.herokuapp.com",
 });
