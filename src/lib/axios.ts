b1ae9c612cdffa44d60b5ad5aa759c689d9f6656
Naiara Martins <naiaraxf@gmail.com>
2024-09-11 10:48:42 -0300

    Update environment baseURL in lib/axios

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,5 +1,7 @@
 import axios from "axios";
 
+console.log('API URL:', process.env.REACT_APP_API_URL);
+
 export const api = axios.create({
   baseURL: process.env.REACT_APP_API_URL || "http://127.0.0.1:8000",
 });
